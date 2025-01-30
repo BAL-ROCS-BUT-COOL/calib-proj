ba41c336ebb842a567109d827a14ff2bb8900944
“Tim Flueckiger <timflueckiger@outlook.com>
2025-01-30 20:59:14 +0100

    .

--- scripts/run_video_generator.py
+++ scripts/run_video_generator.py
@@ -3,14 +3,14 @@ from calib_proj.video_generator.generate_grid import generate_grid
 
 ################# Parameters ###################
 msm_base_size = 18 # [px] must be a multiple of the original size of the dictionary (6 px)
-msm_scales = [5,7]  # scales of the MSMs
+msm_scales = [1,3,5,7]  # scales of the MSMs
 n_grids = 100 # number of MSMs grids
-grid_fps = 10 # number of MSMs grids projected per second
+grid_fps = 10 # number of MSMs grids projected per second (recommended to be <= 3 * min(cameras fps) to avoid temporal synchronization problems)
 
 projector_resolution = (1920, 1080) # [px]
 grid_size = (4, 8) # number of MSMs on x and y axis. (4, 8) is optimized for the aspect ratio of a full HD (1920x1080) projector.
 
-invert_colors = True # invert colors of the markers          
+invert_colors = True # invert colors of MSMs grids        
 ###################################################
 
 n_grids_tot = len(msm_scales) * n_grids
@@ -24,4 +24,4 @@ generate_video(msm_base_size=msm_base_size,
                projector_resolution=projector_resolution, 
                grid_size=grid_size,
                grid_fps=grid_fps, 
-               invert_colors=invert_colors) # folder where the video will be saved
+               invert_colors=invert_colors) 
